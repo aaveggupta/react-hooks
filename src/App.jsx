6525b77f230b3764b30257fd9de0524870bdaa59
aaveggupta <aaveggupta.contact@gmail.com>
2023-08-27 13:27:34 +0530

    useContext hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import UseContextHook from "./UseContextHook/UseContextHook";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <UseContextHook />
     </div>
   );
 }
